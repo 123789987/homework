349a818944fa5b2f16508ecd8bdcdb7b51808a03
123789987 <49760022+123789987@users.noreply.github.com>
2019-10-16 19:21:27 +0800

    Update maze.cpp

--- maze.cpp
+++ maze.cpp
@@ -5,143 +5,166 @@
  *
  */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <vector>
+/*
+insert:
 
-struct pointType {
-  int x;
-  int y;
-};
-struct pathType {
-  int id;
-  struct pointType p;
-  int dir;
-};
-struct pathStack {
-  struct pathType *base;
-  struct pathType *top;
-};
+##########
+#S #   # #
+#  #   # #
+#    ##  #
+# ###    #
+#   #    #
+# #   #  #
+# ### ## #
+##      E#
+##########
+*/
 
-void initMaze(std::vector<std::vector<int> >& maze, bool ifInput) {
-  if (ifInput) {
-    int row, col;
-    printf("Initialize the maze:\nInsert number of rows:");
-    scanf("%i", &row);
-    printf("Insert number of columns:");
-    scanf("%i", &col);
-    printf("Please Insert %i chars\n\"#\" or the number \"1\" means a wall in the maze\na blank or the number\"0\" means a space in the maze\nother chars will be ignored:\n", row*col);
-    int* arr = (int *)malloc((row*col)*sizeof(int));
-    fflush(stdin);
-    for (int i = 0; i < row*col;) {
-      https:
-      char ch = getchar();
-      if (ch == '#' || ch == '1') {
-        arr[i] = 1;
-        i++;
-      } else if (ch == ' ' || ch == '0') {
-        arr[i] = 0;
-        i++;
-      } else {
-        goto https;
-      }
-      
-    }
-    maze.resize(row, std::vector<int>(col));
-    for (int i = 0; i < row; i++) {
-      for (int j = 0; j < col; j++) {
-        maze[i][j] = arr[i*col + j];
-      }
-    }
-  } else {
-    maze.resize(10, std::vector<int>(10));
-    int arr[10][10] = {
-1,1,1,1,1,1,1,1,1,1,
-1,0,0,1,0,0,0,1,0,1,
-1,0,0,1,0,0,0,1,0,1,
-1,0,0,0,0,1,1,0,0,1,
-1,0,1,1,1,0,0,0,0,1,
-1,0,0,0,1,0,0,0,0,1,
-1,0,1,0,0,0,1,0,0,1,
-1,0,1,1,1,0,1,1,0,1,
-1,1,0,0,0,0,0,0,0,1,
-1,1,1,1,1,1,1,1,1,1
-    };
-    for (int i = 0; i < 10; i++) {
-      for (int j = 0; j < 10; j++) {
-        maze[i][j] = arr[i][j];
-      }
-    }
-  }
+
+#include<stdio.h> 
+#include<stdlib.h>
+#include<string.h> 
+#define Maxsize 1010 
+#define Increment 100
+typedef struct{
+  int r, c; 
+}PosType;
+typedef struct{
+  int ord;  
+  PosType seat;
+  int di;
+}SElemType;
+typedef struct{
+  int c;
+  int r;
+  char arr[10][10];
+}MazeType; 
+typedef struct{ 
+  SElemType *base;
+  SElemType *top;
+  int stacksize; 
+}SqStack;
+int Init(SqStack &S){
+  S.base=(SElemType *)malloc(Maxsize*sizeof(SElemType));
+  if(!S.base)  return 0;
+  S.top=S.base;
+  S.stacksize=Maxsize;
+  return 1;
 }
-
-void showMaze(std::vector<std::vector<int> >& maze) {
-  for (int i = 0; i < maze.size(); i++) {
-    for (int j = 0; j < maze[i].size(); j++) {
-      if (maze[i][j] == 1) {
-        printf("%c", '#');
-      } else if (maze[i][j] == 2) {
-        printf("%c", '+');
-      } else if (maze[i][j] == 3) {
-        printf("%c", '*');
-      } else if (maze[i][j] == 4) {
-        printf("%c", '*');
-      } else {
-        printf("%c", ' ');
+int Push(SqStack &S,SElemType x){ 
+  if(S.top-S.base>=S.stacksize){
+    S.base=(SElemType *)realloc(S.base,
+      (S.stacksize+Increment)*sizeof(SElemType));
+      if(!S.base)
+        return 0;
+    S.top=S.base+S.stacksize;
+    S.stacksize+=Increment;
+  } 
+  *S.top++=x;
+  return 1;
+}
+int Pop(SqStack &S, SElemType &x){ 
+  if(S.top==S.base)  return 0;
+  x=*--S.top;
+  return 1;
+}
+bool Pass(MazeType maze, PosType curpos){
+  return maze.arr[curpos.r][curpos.c]==' ' || maze.arr[curpos.r][curpos.c]=='S'
+    || maze.arr[curpos.r][curpos.c]=='E';
+}
+int FootPrint(MazeType &maze, PosType curpos){
+    maze.arr[curpos.r][curpos.c]='o';
+  return 1;
+}
+PosType NextPos(PosType curpos, int di){ 
+  PosType pos=curpos;
+  switch(di)
+  {
+    case 1: pos.c++; break;    //右 
+    case 2: pos.r++; break;    //下 
+    case 3: pos.c--; break;    //左 
+    case 4: pos.r--; break;    //上 
+  }
+  return pos;  
+}
+int MazePath(MazeType &maze, PosType start, PosType end){
+  SqStack S; 
+  PosType curpos;
+  int curstep; 
+  SElemType e; 
+  Init(S);
+  curpos=start;
+  curstep=1;
+  do
+  {
+    if(Pass(maze, curpos)) 
+    {
+      FootPrint(maze, curpos); 
+      e.di = 1;
+            e.ord = curstep;
+            e.seat = curpos; 
+      Push(S, e);
+       if(curpos.c == end.c &&curpos.r == end.r )
+        return 1;
+      curpos=NextPos(curpos, e.di); 
+      curstep++;
+    }
+    else
+    {
+      if(S.top!=S.base)
+      {
+        Pop(S, e);
+        while(e.di==4 && S.top!=S.base) 
+        {
+          maze.arr[e.seat.r][e.seat.c]='!';
+          Pop(S, e);
+        }
+        if(e.di<4)
+        {
+          e.di++;
+          Push(S, e); 
+          curpos=NextPos(e.seat, e.di);
+        }
       }
     }
-    printf("%c", '\n');
+  } while(S.top!=S.base);
+  return 0;
+} 
+int main()
+{
+  int i,j;
+  MazeType maze;
+  maze.r=10, maze.c=10;
+  for(i=0; i<maze.r; i++)
+  {  
+    for(int j=0;j<maze.c; j++)
+      scanf("%c", &maze.arr[i][j]);
+    getchar();
   }
-}
-
-void mazePath(std::vector<std::vector<int> >& maze, int x1, int y1, int x2, int y2) {
-  
-}
-
-int main(int argc, char **argv) {
-  bool ifInput = false;
-  for(int i = 0; i < argc; i++) {
-    if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) {
-      printf("%s","Usage: maze [options]\nOptions:\n  -h                       Display this information.\n  --help                   Display this information.\n  -i                       Refuse to use the default maze.\n  --input                  Refuse to use the default maze.\n");
-      return 0;
-    } else if (strcmp(argv[i], "-i") == 0 || strcmp(argv[i], "--input") == 0) {
-      ifInput = true;
+  PosType start, end;
+  for(i=0; i<10; i++)
+  {
+    for(j=0; j<10; j++)
+    {
+      if(maze.arr[i][j]=='S')
+      {
+        start.r=i;
+        start.c=j;
+      }
+      if(maze.arr[i][j]=='E')
+      {
+        end.r=i;
+        end.c=j;
+      }
     }
   }
-
-  std::vector<std::vector<int> > maze;
-  initMaze(maze, ifInput);
-
-  int x1, x2, y1, y2;
-  if (ifInput) {
-    showMaze(maze);
-      printf("Insert the coordinate of the start and the end point\nTaking the first row and the first column as the origin\nthe start:\nHorizontal axis:x=");
-    scanf("%i", x1);
-    printf("Vertical axis:y=");
-    scanf("%i", y1);
-    printf("the end:\nHorizontal axis:x=");
-    scanf("%i", x2);
-    printf("Vertical axis:y=");
-    scanf("%i", y2);
-  } else {
-    x1 = 1;
-    y1 = 1;
-    x2 = 8;
-    y2 = 8;
+  MazePath(maze, start, end);
+  for(i=0; i<maze.r; i++)
+  {
+    for(j=0; j<maze.c; j++)
+      printf("%c", maze.arr[i][j]);
+    printf("\n");
   }
-  maze[y1][x1] = 3;
-  maze[y2][x2] = 4;
-
-  showMaze(maze);
-
-  mazePath(maze, x1, y1, x2, y2);
-
-  printf("result:\n");
-  showMaze(maze);
-
-  fflush(stdin);
-  printf("Press any key to continue...");
-  getchar();
+  printf("%i--%i", sizeof(char), sizeof(int));
   return 0; 
 }
